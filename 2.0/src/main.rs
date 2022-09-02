a63d21019369ccadbaa6235415efcd53ec9aa06d
TomiXRM <chocolate0785lego@icloud.com>
2022-09-02 12:29:27 +0900

    RustにはStringとstr型があるらしい

--- 2.0/src/main.rs
+++ 2.0/src/main.rs
@@ -1,12 +1,19 @@
 fn main(){
-    let x : f32 = 7.0;
-    let y : f64 = 0.3;
-    println!("x*y={}",x as f64 *y);//型が異なるのでエラーだがas f64ってすればいけるらしい
-    //no implementation for f32 * f64
+    //RustにはStringとstr型があるらしい
+    //strは文字列スライスっていうらしい。
+    //strではメモリ上の文字列の先頭と長さを保持しているらしい
 
 
-    let c:char = 'c';
-    let b = b'c';//b'なんとか'をすると「なんとか」の文字が整数に直される(ASCII)
+    // strを表示
+    let msg:&str = "Hello world!!" ;
+    println!("msg={}",msg);
 
-    println!("{} is {} in integer",c,b);
+
+    //文字スライスの長さはlen()を使って取得できるらしい
+    println!("len{}",msg.len());
+    
+
+
+    let s = &msg[2..5]; // 3文字目(0スタート)から5文字目までを取得
+    println!("3..5={}",s);
 }
