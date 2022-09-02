06ba6e4d02536bb3cedc3b9dccdbb8c1d9166962
TomiXRM <chocolate0785lego@icloud.com>
2022-09-02 13:35:47 +0900

    unsafeとstatic

--- 2.0/src/main.rs
+++ 2.0/src/main.rs
@@ -1,11 +1,25 @@
-fn main(){
-    let x = 12;
-    println!("{}",x);
-    let x = 9;
-    println!("{}",x);
-    //変数は他のlet文があるまで有効らしい
+//static 変数
+// Staticな変数はどこからでも参照することができる変数らしい。
+// ただし大文字で宣言することtと、型を明示する必要がある。
+static mut VAL : i32 = 123;
 
-    //output
-    // 12
-    // 9
+// staticは便利なように思えますが、重大なバグの原因になる可能性を持っているので
+// unsafeで囲むことにしている。
+fn main(){
+    unsafe{
+        println!("VAL={}",VAL);
+    }
+
+    twice();
+
+    unsafe{
+        println!("VAL={}",VAL);
+    }
+}
+
+fn twice(){
+    unsafe{
+        VAL = VAL * 2;
+        println!("VAL in twice() = {}",VAL);
+    }
 }
