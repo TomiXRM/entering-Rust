06c71207c8f11831b43df3e2c2ec0ad96473e556
Daiki Tomioka <chocolate0785lego@icloud.com>
2022-09-03 00:59:11 +0900

    入力と条件分岐

--- new3_3/src/main.rs
+++ new3_3/src/main.rs
@@ -1,15 +1,15 @@
 use std::io;
 fn main() {
-    println!("enter a number");
-    let mut s: String = String::new();
-    let rslt = io::stdin().read_line(&mut s);
-    match rslt {
-        Ok(_v) => {}
-        Err(e) => {
-            println!("{}", e,);
-        }
+    println!("整数を入力して下さい");
+    //標準入力から1行を読み取り,整数にする
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).ok();
+    let x: i32 = s.trim().parse().unwrap();
+    if x > 0 {
+        println!("{}は正の数です", x);
+    } else if x < 0 {
+        println!("{}は負の数です", x);
+    } else {
+        println!("{}はzeroです", x);
     }
-
-    let num: i16 = s.trim().parse().unwrap();//trim()がないと改行で死ぬっぽ
-    println!("{} * 2 = {}", num, num * 2);
 }
