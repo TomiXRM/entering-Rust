47e349c4aed92a5ab37d8970cddab22819de13fb
TomiXRM <chocolate0785lego@icloud.com>
2022-09-02 17:28:58 +0900

    useを使ってstd::ioのやつを標準で使えるようにした

--- 2.0/src/main.rs
+++ 2.0/src/main.rs
@@ -1,9 +1,11 @@
 //コンソール入力
+use std::io;
+
 fn main() {
     println!("整数を入力してください");
-    //標準入力から1行読み取り、整数にする
     let mut s = String::new();
-    std::io::stdin().read_line(&mut s).ok(); //文字の読み取り
-    let n: i32 = s.trim().parse().ok().unwrap(); //整数に変換する
-    println!("{}の2倍は{}", n, n * 2);
+    io::stdin().read_line(&mut s).ok();
+    let n: i32 = s.trim().parse().ok().unwrap();
+
+    println!("{}の2倍の値は{}", n, n * 2);
 }
