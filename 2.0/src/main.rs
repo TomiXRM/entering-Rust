c422f783d50bca6786d2553e9436710b3d2f12f9
TomiXRM <chocolate0785lego@icloud.com>
2022-09-02 17:23:46 +0900

    文字を読み取って整数に変換する

--- 2.0/src/main.rs
+++ 2.0/src/main.rs
@@ -1,9 +1,9 @@
 //コンソール入力
 fn main() {
-    println!("名前を入力してください！");
-    let mut line = String::new();
-    std::io::stdin().read_line(&mut line).ok();
-
-    let name = line.trim().to_string();
-    println!("こんにちは{}さん", name);
+    println!("整数を入力してください");
+    //標準入力から1行読み取り、整数にする
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok(); //文字の読み取り
+    let n: i32 = s.trim().parse().ok().unwrap(); //整数に変換する
+    println!("{}の2倍は{}", n, n * 2);
 }
