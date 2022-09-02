8a7240adb0edb20f2aac4bd24d63a6b3f8c046da
TomiXRM <chocolate0785lego@icloud.com>
2022-09-02 17:18:16 +0900

    さっきのプログラムにRust analyzerをかけた

--- 2.0/src/main.rs
+++ 2.0/src/main.rs
@@ -1,9 +1,9 @@
 //コンソール入力
-fn main(){
+fn main() {
     println!("名前を入力してください！");
     let mut line = String::new();
     std::io::stdin().read_line(&mut line).ok();
 
     let name = line.trim().to_string();
-    println!("こんにちは{}さん",name);
+    println!("こんにちは{}さん", name);
 }
