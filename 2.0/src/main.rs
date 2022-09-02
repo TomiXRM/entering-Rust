c97a4fb4083a53462f1a11708940ff2614c977ae
TomiXRM <chocolate0785lego@icloud.com>
2022-09-02 13:26:13 +0900

    所有権

--- 2.0/src/main.rs
+++ 2.0/src/main.rs
@@ -1,19 +1,6 @@
 fn main(){
-    //RustにはStringとstr型があるらしい
-    //strは文字列スライスっていうらしい。
-    //strではメモリ上の文字列の先頭と長さを保持しているらしい
-
-
-    // strを表示
-    let msg:&str = "Hello world!!" ;
-    println!("msg={}",msg);
-
-
-    //文字スライスの長さはlen()を使って取得できるらしい
-    println!("len={}",msg.len());
-    
-
-
-    let s = &msg[2..5]; // 3文字目(0スタート)から5文字目までを取得
-    println!("3..5={}",s);
+    //所有権
+    let s1 = String ::from("hello");
+    let s2 = s1.clone();
+    println!("s1={},s2={}",s1,s2);
 }
