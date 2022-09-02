0894c798f23b4b4bb600b5e78350fcb8968a47cd
TomiXRM <chocolate0785lego@icloud.com>
2022-09-02 12:15:38 +0900

    型が異なると掛け算もできないらしいf32とf64

--- 2.0/src/main.rs
+++ 2.0/src/main.rs
@@ -1,6 +1,8 @@
 fn main(){
-    let x = 7.0;
-    println!("{}",type_of(x));
+    let x : f32 = 7.0;
+    let y : f64 = 0.3;
+    println!("x*y={}",x*y);//型が異なるのでエラー
+    //no implementation for f32 * f64
 }
 
 fn type_of<T>(_:T) -> &'static str{
