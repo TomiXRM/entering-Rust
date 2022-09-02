b3fad500a2f8a73a1e4a9d5894ad05d9cb812e95
TomiXRM <chocolate0785lego@icloud.com>
2022-09-02 17:42:08 +0900

    matchとresultについてちょっとやった

--- 2.0/src/main.rs
+++ 2.0/src/main.rs
@@ -3,9 +3,17 @@ use std::io;
 
 fn main() {
     println!("整数を入力してください");
+    //標準入力から1行を読み取り、整数にする
     let mut s = String::new();
-    io::stdin().read_line(&mut s).ok();
-    let n: i32 = s.trim().parse().ok().unwrap();
 
-    println!("{}の2倍の値は{}", n, n * 2);
+    let rslt = io::stdin().read_line(&mut s); //読み取り
+
+    match rslt {
+        //エラーがあればチェック
+        Ok(v) => println!("読みとり成功:{:?}", v),
+        Err(e) => println!("読み取り失敗:{:?}", e),
+    } //ちょっと関数型っぽさ
+
+    let n: i32 = s.trim().parse().ok().unwrap(); //ここで文字がきた時に対処考えてみたい
+    println!("{}", n);
 }
