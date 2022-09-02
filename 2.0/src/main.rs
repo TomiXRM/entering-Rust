2e0659bdaf2551ae47dd5edd47b4c0f8ecf5e101
TomiXRM <chocolate0785lego@icloud.com>
2022-09-02 16:06:03 +0900

    進数表示

--- 2.0/src/main.rs
+++ 2.0/src/main.rs
@@ -1,16 +1,9 @@
 //キャスト変換?
 fn main(){
-    // let x : i32 = 456;
-    // let ref r = &x;
-    // println!("{} {} {:?}",x,r,*r);
-
-    let x : i32 = 456;
-    let ref r = x;
-    println!("{} is typeof {}",x,type_of(x));
-    println!("{} is typeof {}",r,type_of(r));
-    println!("{} is typeof {}",*r,type_of(*r));
-}
-
-fn type_of<T>(_:T) -> & 'static str{
-    std::any::type_name::<T>()
+    println!("{:b}",123);  //2進数
+    println!("{:o}",123);  //9進数
+    println!("{:x}",123);  //16進数
+    println!("{:X}",123);  //16進数(大文字)
+    println!("{:e}",123);  //1.23e1
+    println!("{:E}",123);  //1.23E1
 }
