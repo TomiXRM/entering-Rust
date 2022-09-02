974c71bd7d94a3240e35c7b110ca98cc88274768
Daiki Tomioka <chocolate0785lego@icloud.com>
2022-09-03 01:09:35 +0900

    matchで奇数偶数判定

--- new3_3/src/main.rs
+++ new3_3/src/main.rs
@@ -1,15 +1,13 @@
 use std::io;
 fn main() {
-    println!("整数を入力して下さい");
-    //標準入力から1行を読み取り,整数にする
-    let mut s = String::new();
+    println!("数値を入力してください");
+    let mut s: String = String::new();
     io::stdin().read_line(&mut s).ok();
-    let x: i32 = s.trim().parse().unwrap();
-    if x > 0 {
-        println!("{}は正の数です", x);
-    } else if x < 0 {
-        println!("{}は負の数です", x);
-    } else {
-        println!("{}はzeroです", x);
+    let n: i32 = s.trim().parse().unwrap();
+    let x: i32 = n % 2;
+    match x {
+        0 => println!("{}は偶数です", n),
+        1 => println!("{}は奇数です", n),
+        _ => println!("{}は奇数でも偶数でもありません", n),
     }
 }
