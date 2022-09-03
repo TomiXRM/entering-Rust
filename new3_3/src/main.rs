5b6151350d9594543ac4dd932e7bd27868f50af0
Daiki Tomioka <chocolate0785lego@icloud.com>
2022-09-03 14:23:16 +0900

    奇数偶数判定

--- new3_3/src/main.rs
+++ new3_3/src/main.rs
@@ -1,13 +1,19 @@
 use std::io;
 fn main() {
-    println!("数値を入力してください");
+    println!("type number");
     let mut s: String = String::new();
-    io::stdin().read_line(&mut s).ok();
+    let r = io::stdin().read_line(&mut s);
+    match r {
+        Ok(_) => {}
+        Err(_) => {
+            println!("type again");
+            return;
+        }
+    }
     let n: i32 = s.trim().parse().unwrap();
-    let x: i32 = n % 2;
-    match x {
-        0 => println!("{}は偶数です", n),
-        1 => println!("{}は奇数です", n),
-        _ => println!("{}は奇数でも偶数でもありません", n),
+    let b: bool = (n % 2) == 0;
+    match b {
+        true => println!("奇数です"),
+        false => println!("偶数です"),
     }
 }
