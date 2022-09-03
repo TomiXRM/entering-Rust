c00b8af9d12dc120894e811a039d2b817e12718e
Daiki Tomioka <chocolate0785lego@icloud.com>
2022-09-03 14:44:12 +0900

    えらーはんどりんぐどうしたらいいかわからんけど、こうやったらcontinueいけるんちゃうんかと思っ

--- new3_3/src/main.rs
+++ new3_3/src/main.rs
@@ -1,11 +1,22 @@
-// use std::io;
+use std::io;
 fn main() {
-    'looptop: for i in 0..4 {
-        for j in 0..4 {
-            if i == 2 && j == 2 {
-                break 'looptop;
+    println!("type number");
+    loop {
+        let mut s: String = String::new();
+        let r = io::stdin().read_line(&mut s);
+        match r {
+            Ok(_) => {}
+            Err(_) => {
+                println!("type again");
+                continue;
             }
-            println!("{}{}", i, j);
         }
+        let n: i32 = s.trim().parse().unwrap();
+        let b: bool = (n % 2) == 0;
+        match b {
+            true => println!("奇数です"),
+            false => println!("偶数です"),
+        }
+        loop {}
     }
 }
